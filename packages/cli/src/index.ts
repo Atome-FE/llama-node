554eac86c1dc6881efbbb6b8f49da22c44defaa9
hlhr202 <hlhr202@163.com>
2023-04-08 02:25:18 +0800

    feat: init cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
-import { convert } from "@llama-node/core";
+import {
+    convert,
+    LLamaInferenceArguments,
+    LLama,
+    LLamaConfig,
+} from "@llama-node/core";
 import yargs from "yargs";
 import path from "path";
 import { existsSync } from "fs";
@@ -8,9 +13,86 @@ const convertType = ["q4_0", "q4_1", "f16", "f32"] as const;
 
 type ConvertType = typeof convertType[number];
 
-class ConvertCommand implements yargs.CommandModule {
+interface CLIInferenceArguments extends LLamaInferenceArguments, LLamaConfig {
+    logger?: boolean;
+}
+
+class InferenceCommand implements yargs.CommandModule {
+    command = "inference";
+    describe = "Inference LLaMA";
+    builder(args: yargs.Argv) {
+        return (args as yargs.Argv<CLIInferenceArguments>)
+            .help("help")
+            .example('llama inference -p "How are you?"', "Inference LLaMA")
+            .options("feedPrompt", {
+                type: "boolean",
+                demandOption: false,
+                description: "Set it to true to hide promt feeding progress",
+            })
+            .options("float16", { type: "boolean", demandOption: false })
+            .options("ignoreEos", { type: "boolean", demandOption: false })
+            .options("nBatch", { type: "number", demandOption: false })
+            .options("nThreads", { type: "number", demandOption: false })
+            .options("numPredict", { type: "number", demandOption: false })
+            .options("prompt", {
+                type: "string",
+                demandOption: true,
+                alias: "p",
+            })
+            .options("repeatLastN", { type: "number", demandOption: false })
+            .options("repeatPenalty", { type: "number", demandOption: false })
+            .options("seed", { type: "number", demandOption: false })
+            .options("temp", { type: "number", demandOption: false })
+            .options("tokenBias", { type: "string", demandOption: false })
+            .options("topK", { type: "number", demandOption: false })
+            .options("topP", { type: "number", demandOption: false })
+            .options("path", {
+                type: "string",
+                demandOption: true,
+                alias: ["m", "model"],
+            })
+            .options("numCtxTokens", { type: "number", demandOption: false })
+            .options("logger", {
+                type: "boolean",
+                demandOption: false,
+                default: true,
+                alias: "verbose",
+            });
+    }
+    async handler(args: yargs.ArgumentsCamelCase) {
+        const {
+            $0,
+            _,
+            path: model,
+            numCtxTokens,
+            logger,
+            ...rest
+        } = args as yargs.ArgumentsCamelCase<CLIInferenceArguments>;
+        const absolutePath = path.isAbsolute(model)
+            ? model
+            : path.join(process.cwd(), model);
+        if (logger) {
+            LLama.enableLogger();
+        }
+        const llama = LLama.create({ path: absolutePath, numCtxTokens });
+        llama.inference(rest, (result) => {
+            switch (result.type) {
+                case "DATA":
+                    process.stdout.write(result.data.token);
+                    break;
+                case "ERROR":
+                    console.error(result.message);
+                    break;
+                case "END":
+                    break;
+            }
+        });
+    }
+}
+
+class ConvertCommand implements yargs.CommandModule<any, any> {
     command = "convert";
-    describe = "Convert llama pth to ggml";
+    describe = "Convert llama pth to ggml, not ready yet";
     builder(args: yargs.Argv) {
         return args
             .help("help")
@@ -34,7 +116,7 @@ class ConvertCommand implements yargs.CommandModule {
                 },
             });
     }
-    async handler(args: yargs.Arguments) {
+    async handler(args: yargs.ArgumentsCamelCase) {
         const dir = args.dir as string;
         const type = args.type as ConvertType;
 
@@ -52,9 +134,11 @@ class ConvertCommand implements yargs.CommandModule {
     }
 }
 
-yargs
+(yargs as yargs.Argv<any | CLIInferenceArguments>)
     .scriptName("llama")
     .usage("$0 <cmd> [args]")
     .command(new ConvertCommand())
+    .command(new InferenceCommand())
     .demandCommand(1, "You need at least one command before moving on")
+    .strict()
     .parse();
