7952d3cfec5c2913ab6584d2c999648eb4b30e64
hlhr202 <hlhr202@163.com>
2023-04-24 15:53:58 +0800

    chore: add more docs

---
+++ packages/docs/scripts/write-example.ts
@@ -0,0 +1,33 @@
+import fs from "fs";
+import path from "path";
+import * as glob from "glob";
+
+const examplePath = path.resolve(__dirname, "../../../example");
+
+const writeExample = (variant: string) => {
+    const json = glob
+        .sync(path.resolve(examplePath, `js/${variant}/*.js`))
+        .map((file) => {
+            const fileName = path.basename(file);
+            const fileNameWithoutExtension = fileName.split(".")[0];
+            const fileContent = fs.readFileSync(file, "utf-8");
+
+            const markdown = `// ${fileName}
+${fileContent}`;
+
+            return {
+                name: fileNameWithoutExtension,
+                markdown,
+            };
+        });
+
+    const destination = path.resolve(__dirname, "../src/components/example");
+
+    fs.writeFileSync(
+        path.resolve(destination, `./${variant}.json`),
+        JSON.stringify(json, null, 2)
+    );
+};
+
+writeExample("llama-cpp");
+writeExample("llama-rs");
