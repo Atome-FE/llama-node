a311873d6b5efb465be9402827119d52f0550e38
hlhr202 <hlhr202@163.com>
2023-05-08 23:10:15 +0800

    fix: dependabot alerts

--- packages/hf-tokenizer/src/lib.rs
+++ packages/hf-tokenizer/src/lib.rs
@@ -2,12 +2,6 @@ use std::str::FromStr;
 use tokenizers::tokenizer::{Encoding as HfEncoding, Tokenizer as HfTokenizer};
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-// #[cfg(feature = "wee_alloc")]
-// #[global_allocator]
-// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub struct Tokenizer {
     tokenizer: HfTokenizer,
