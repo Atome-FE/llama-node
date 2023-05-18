844e0831ae31d7f83fb31dd77d88d1a4a553f07d
hlhr202 <hlhr202@163.com>
2023-05-18 16:20:01 +0800

    update: better logger

--- packages/common-rs/src/logger.rs
+++ packages/common-rs/src/logger.rs
@@ -1,5 +1,7 @@
-use log::{LevelFilter, Log, Metadata, Record};
+use console::style;
+use log::{Level, LevelFilter, Log, Metadata, Record};
 use once_cell::sync::Lazy;
+use time::{format_description, OffsetDateTime};
 
 pub struct LLamaLogger {
     enabled: bool,
@@ -20,18 +22,37 @@ impl LLamaLogger {
     pub fn get_singleton() -> &'static mut LLamaLogger {
         unsafe { &mut LLAMA_LOGGER }
     }
+
+    pub fn style_level(&self, level: Level) -> console::StyledObject<&str> {
+        match level {
+            Level::Error => style("ERROR").red(),
+            Level::Warn => style("WARN").yellow(),
+            Level::Info => style("INFO").green(),
+            Level::Debug => style("DEBUG").blue(),
+            Level::Trace => style("TRACE").cyan(),
+        }
+    }
 }
 
 impl Log for LLamaLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
         metadata.level() <= log::Level::Info
-        // true
     }
 
     fn log(&self, record: &Record) {
         // Check if the record is matched by the logger before logging
         if self.enabled(record.metadata()) && self.enabled {
-            println!("{} - {}", record.level(), record.args());
+            let time = OffsetDateTime::now_utc()
+                .format(&format_description::well_known::Rfc2822)
+                .unwrap();
+
+            println!(
+                "[{} - {} - {}] - {}",
+                time,
+                self.style_level(record.level()),
+                record.module_path().unwrap_or("unknown"),
+                record.args()
+            );
         }
     }
 
