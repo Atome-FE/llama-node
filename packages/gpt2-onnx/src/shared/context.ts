cd6bd69559040f0db0b08af508e39caa4839b433
hlhr202 <hlhr202@163.com>
2023-05-29 13:47:22 +0800

    update: use alias for isomorphic import

--- packages/gpt2-onnx/src/shared/context.ts
+++ packages/gpt2-onnx/src/shared/context.ts
@@ -1,4 +1,5 @@
-import { loadOnnx, loadTensorFlow } from "./loader";
+import * as tfjs from "@tensorflow/tfjs-node";
+import * as onnx from "onnxruntime-node";
 import type { Rank } from "@tensorflow/tfjs";
 import type { IsomorphicTokenizer } from "./tokenizer";
 import type { InferenceSession, TypedTensor } from "onnxruntime-node";
@@ -20,19 +21,14 @@ interface IGPT2OnnxInferenceOptions {
 export class IsomorphicContext {
     tokenizer?: IsomorphicTokenizer;
     session?: InferenceSession;
-    tfjs?: Awaited<ReturnType<typeof loadTensorFlow>>;
-    onnx?: Awaited<ReturnType<typeof loadOnnx>>;
 
     static async create(options: IGPT2OnnxOptions) {
         const tokenizer = options.tokenizer;
         await tokenizer.initFromUrl(options.tokenizerUrl);
         const gpt2Onnx = new IsomorphicContext();
 
-        gpt2Onnx.onnx = await loadOnnx();
-        gpt2Onnx.tfjs = await loadTensorFlow();
-
         gpt2Onnx.tokenizer = tokenizer;
-        gpt2Onnx.session = await gpt2Onnx.onnx.InferenceSession.create(
+        gpt2Onnx.session = await onnx.InferenceSession.create(
             options.modelPath as ArrayBufferLike
         );
 
@@ -44,10 +40,7 @@ export class IsomorphicContext {
     }
 
     getLogits(onnxTensor: TypedTensor<"float32">) {
-        if (!this.tfjs) {
-            throw new Error("Tensorflow not initialized");
-        }
-        let output = this.tfjs
+        let output = tfjs
             .tensor<Rank.R3>(onnxTensor.data, onnxTensor.dims as any)
             .slice(0, 1);
 
@@ -60,9 +53,6 @@ export class IsomorphicContext {
     }
 
     async inference(inferArgs: IGPT2OnnxInferenceOptions) {
-        if (!this.tfjs) {
-            throw new Error("Tensorflow not initialized");
-        }
         if (!this.tokenizer) {
             throw new Error("Tokenizer not initialized");
         }
@@ -74,21 +64,20 @@ export class IsomorphicContext {
         const numPredict = inferArgs.numPredict ?? 128;
         const topK = inferArgs.topK ?? 1;
 
-        let text = inferArgs.prompt;
         let remain = numPredict;
+        const tokens = this.tokenizer.tokenize(inferArgs.prompt, true);
 
         while (remain > 0) {
             remain -= 1;
 
-            // TODO: optimize to reuse input_ids and attention_mask instead of creating new ones from text
-            const inputs = this.tokenizer.tokenize(text, true);
+            const inputs = this.tokenizer.toOnnx(tokens);
             const result = await this.session.run(inputs);
 
             const logits = this.getLogits(
                 result["last_hidden_state"] as TypedTensor<"float32">
             );
 
-            let probs = this.tfjs.softmax(logits, -1);
+            let probs = tfjs.softmax(logits, -1);
 
             // TODO: implement topP
             probs = probs.topk(topK, true).indices.slice(0, 1).squeeze();
@@ -111,8 +100,7 @@ export class IsomorphicContext {
             );
 
             inferArgs.onProgress(tokenText);
-
-            text += tokenText;
+            tokens.push(BigInt(token[0]));
         }
     }
 }
--- packages/gpt2-onnx/src/shared/tokenizer.ts
+++ packages/gpt2-onnx/src/shared/tokenizer.ts
@@ -1,16 +1,14 @@
-import type { Tokenizer } from "@llama-node/hf-tokenizer";
+import { Tokenizer } from "@llama-node/hf-tokenizer/nodejs/tokenizer-node";
+import { Tensor } from "onnxruntime-node";
+import axios from "axios";
 import type { TensorConstructor } from "onnxruntime-node";
-import { loadHfTokenizer, loadOnnx } from "./loader";
 
 export class IsomorphicTokenizer {
     tokenizer?: Tokenizer;
     Tensor?: TensorConstructor;
 
     async initFromUrl(url: string) {
-        const json = await (await fetch(url)).json();
-
-        const { Tokenizer } = await loadHfTokenizer();
-        const { Tensor } = await loadOnnx();
+        const json = await axios.get(url).then((res) => res.data);
 
         this.Tensor = Tensor;
         this.tokenizer = new Tokenizer(JSON.stringify(json));
@@ -24,25 +22,19 @@ export class IsomorphicTokenizer {
         return this.tokenizer?.decode(ids, skipSpecialTokens) ?? "";
     }
 
-    tokenize(text: string, addSpecialTokens = true) {
+    toOnnx(input: bigint[]) {
         if (!this.Tensor) {
             throw new Error("Onnx is not initialized");
         }
 
-        const encoded = this.tokenizer?.encode(text, addSpecialTokens);
+        const inputArray = BigInt64Array.from(input);
 
-        const inputIdArray = BigInt64Array.from(
-            Array.from(encoded?.ids ?? []).map((x) => BigInt(x))
-        );
-
-        encoded?.free();
-
-        const input_ids = new this.Tensor("int64", inputIdArray, [
+        const input_ids = new this.Tensor("int64", inputArray, [
             1,
-            inputIdArray.length,
+            inputArray.length,
         ]);
 
-        const attentionMaskArray = inputIdArray.map(() => BigInt(1));
+        const attentionMaskArray = inputArray.map(() => BigInt(1));
 
         const attention_mask = new this.Tensor("int64", attentionMaskArray, [
             1,
@@ -50,5 +42,21 @@ export class IsomorphicTokenizer {
         ]);
 
         return { input_ids, attention_mask };
+    }
+
+    tokenize(text: string, addSpecialTokens = true) {
+        if (!this.Tensor) {
+            throw new Error("Onnx is not initialized");
+        }
+
+        const encoded = this.tokenizer?.encode(text, addSpecialTokens);
+
+        const inputIdArray = Array.from(encoded?.ids ?? []).map((x) =>
+            BigInt(x)
+        );
+
+        encoded?.free();
+
+        return inputIdArray;
     }
 }
